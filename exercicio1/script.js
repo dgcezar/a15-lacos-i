bd6fb0975be599b1af9a3d1b02a692d785cdd186
dgcezar <diego.cezar13@gmail.com>
2022-09-12 11:55:34 -0300

    att2 - exercício 3 finalizado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,12 +1,11 @@
 let coxinhas = prompt(
-  "Deseja comer mais coxinhas? (responda 'S' para sim ou 'N' para não)"
-);
+  "Deseja comer mais coxinhas? (responda 'S' para sim ou 'N' para não)").toUpperCase();
 
 let conta = 0;
 
 while (coxinhas === "S") {
   conta = conta + 2.5;
-  coxinhas = prompt("Deseja comer mais coxinhas?");
+  coxinhas = prompt("Deseja comer mais coxinhas?").toUpperCase();
 }
 
 console.log(`Valor atual da conta: R$ ${conta}`);
---
+++ exercicio3/script.js
@@ -0,0 +1,5 @@
+const campeoesDoMundo = ["Brasil", "Alemanha", "Itália", "Argentina", "Uruguai"];
+
+for (let i = 0; i < campeoesDoMundo.length; i++) {
+    console.log(`${i+1} - ${campeoesDoMundo[i]}`);
+}
